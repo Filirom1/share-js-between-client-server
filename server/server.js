977921a3e03c4aa8eafddb8f9a2e0a7c037ae1bd
Filirom1 <filirom1@gmail.com>
2013-11-25 19:25:19 +0100

    make it works on windows

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var lib = require('../common/lib');
 var app = express();
 
 app.use('/', express.static(__dirname + '/../client'))
+app.use('/js/common', express.static(__dirname + '/../common'))
 
 app.get('/hello', function(req, res){
   res.send('' + lib.hello());
